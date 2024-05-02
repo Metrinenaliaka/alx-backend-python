6d02060d8e23b01ad01d59958e1bcc11673d582b
Metrinenaliaka <metrinenam@gmail.com>
2024-05-02 16:18:42 +0300

    fixed errors

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,13 +3,8 @@
 Type-annotated function element_length
 """
 
-from typing import Sequence, Union, List, Tuple
+from typing import Iterable, Tuple
 
 
-def element_length(
-    lst: Union[Sequence[Union[List[int], Tuple[int, int]]]]
-) -> List[Tuple[Union[List[int], Tuple[int, int]], int]]:
-    """
-    Return list of tuples where each tuple contains a sequence and its length
-    """
+def element_length(lst: Iterable[str]) -> Iterable[Tuple[str, int]]:
     return [(i, len(i)) for i in lst]
