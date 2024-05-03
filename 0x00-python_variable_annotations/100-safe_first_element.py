835e1ec9fdfdf698a14b3cc9110984920ef9b302
Metrinenaliaka <metrinenam@gmail.com>
2024-05-03 17:03:24 +0300

    fixed errors

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,8 +3,11 @@
 Type-annotated function safe_first_element
 """
 
-from typing import Any, Optional
+from typing import Any, Optional, List
 
 
-def safe_first_element(lst: list) -> Optional[Any]:
+def safe_first_element(lst: List[Any]) -> Optional[Any]:
+    """
+    Return the first element of a list
+    """
     return lst[0] if lst else None
