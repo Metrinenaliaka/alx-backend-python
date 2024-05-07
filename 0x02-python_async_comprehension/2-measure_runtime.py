f6122137cd15a397b8ccb0c7ed6a6507a75a834d
Metrinenaliaka <metrinenam@gmail.com>
2024-05-07 22:04:54 +0300

    errors

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,16 +5,28 @@ measure_runtime coroutine that will execute measure_time
 """
 
 import asyncio
-import time
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """
-    measure_runtime coroutine that will execute measure_time
-    4 times in parallel using asyncio.gather
+    A coroutine that executes async_comprehension
     """
-    start_time = time.time()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end_time = time.time()
+    start_time = asyncio.get_event_loop().time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_time = asyncio.get_event_loop().time()
     return end_time - start_time
+
+
+if __name__ == "__main__":
+    runtime = asyncio.run(measure_runtime())
+    print(f"Total runtime: {runtime} seconds")
